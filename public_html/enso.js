dc4585d9ebdfe5283f6dae7d30979bd752bbab33
daton <rapidclimate@gmail.com>
2017-04-04 14:06:27 -0500

    asasas

--- public_html/enso.js
+++ public_html/enso.js
@@ -16,3 +16,39 @@ d3.json("https://www.ncdc.noaa.gov/teleconnections/enso/indicators/soi/data.json
 //Funciones basicas
 parseInt("77");
 parseFloat("3.14");
+
+
+
+
+var data = [10, 15, 30, 50, 80, 65, 55, 30, 20, 10, 8]; 
+    function render(data) { // <- B
+        // Enter
+        d3.select("body").selectAll("div.h-bar") 
+            .data(data) 
+            .enter() 
+                .append("div") 
+                    .attr("class", "h-bar")
+                .append("span"); 
+        // Update
+        d3.select("body").selectAll("div.h-bar")
+            .data(data) 
+                .style("width", function (d) { 
+                    return (d * 3) + "px";
+                })
+                .select("span") 
+                    .text(function (d) {
+                        return d;
+                    });
+                
+     
+        d3.select("body").selectAll("div.h-bar")
+            .data(data)
+            .exit() 
+                .remove();        
+    }
+    setInterval(function () { 
+        data.shift();
+        data.push(Math.round(Math.random() * 100));
+        render(data);
+    }, 1500);
+    render(data);
